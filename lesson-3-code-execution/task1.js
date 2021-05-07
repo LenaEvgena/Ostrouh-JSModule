cc568427286fbfe00f6216e0f605c2a759bba27b
Alena Astravukh <helen151204@gmail.com>
2021-05-07 12:02:56 +0300

    feat: correct syntax

--- lesson-3-code-execution/task1.js
+++ lesson-3-code-execution/task1.js
@@ -7,18 +7,18 @@ var tasksCompleted = {
   'Anton': 99
   };
 
-  function bestWorker(obj) {
-    var tasksArray = Object.entries(obj);
-    var bestNumber = 0;
-    var bestName = '';
+function bestWorker(obj) {
+  var tasksArray = Object.entries(obj);
+  var bestNumber = 0;
+  var bestName = '';
 
-    for(var [key, value] of tasksArray) {
-      if(value > bestNumber) {
-        bestNumber = value;
-        bestName = key;
-      }
+  for(var [key, value] of tasksArray) {
+    if(value > bestNumber) {
+      bestNumber = value;
+      bestName = key;
     }
-    return bestName;
   }
+  return bestName;
+}
 
-  console.log( bestWorker(tasksCompleted) );
+console.log( bestWorker(tasksCompleted) );
