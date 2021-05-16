dd867dbd9e04cb3ef501c5511a8983c2d7a1cc5d
Alena Astravukh <helen151204@gmail.com>
2021-05-16 15:27:59 +0300

    feat: add changes

--- lesson-4-closures/task1.js
+++ lesson-4-closures/task1.js
@@ -1,21 +1,21 @@
 'use strict';
 
 function createDesk(width, height) {
-	var desk = '';
-	for (var i = 0; i < width; i++) {
-		var substr = '';
-		var br = '\n';
-		for (var j = 0; j < height; j++) {
-			if ((i + j) % 2 === 0) {
-				substr += '#';
-			} else {
-				substr += ' ';
-			}
-		}
-		substr += br;
-		desk += substr;
-	}
-	return desk;
+  var desk = '';
+  for (var i = 0; i < width; i++) {
+    var substr = '';
+    var br = '\n';
+    for (var j = 0; j < height; j++) {
+      if ((i + j) % 2 === 0) {
+        substr += '#';
+      } else {
+        substr += ' ';
+      }
+    }
+    substr += br;
+    desk += substr;
+  }
+  return desk;
 }
 
 console.log( createDesk(8, 8) );
