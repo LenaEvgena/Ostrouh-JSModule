86d706ab832388110099a546759b55399c9fe3a1
Alena Astravukh <helen151204@gmail.com>
2021-05-16 11:11:19 +0300

    feat: add content

--- lesson-4-closures/task2.js
+++ lesson-4-closures/task2.js
@@ -1,22 +1,21 @@
 'use strict';
 
 var classObj = {
-	className: 'menu open open my',
+  className: 'menu open open my',
 };
 
 function removeExistingClass (obj, cls) {
-	var classArray = obj.className.split(' ');
-	var result = [];
-	for (var i = 0; i < classArray.length; i++) {
-		if (classArray[i] === cls) {
-			continue;
-		} else {
-			result.push(classArray[i]);
-		}
-	}
-	return obj.className = result.join(' ').trim();
+  var classArray = obj.className.split(' ');
+  var result = [];
+  for (var i = 0; i < classArray.length; i++) {
+    if (classArray[i] === cls) {
+      continue;
+    } else {
+      result.push(classArray[i]);
+    }
+  }
+  return obj.className = result.join(' ').trim();
 }
 
 removeExistingClass(classObj, 'open');
 console.log(classObj.className);
-
