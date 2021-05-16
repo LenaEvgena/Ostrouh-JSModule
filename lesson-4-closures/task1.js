e0cda70bad165c6b8ea83de48f580c4c4d0dfee4
Alena Astravukh <helen151204@gmail.com>
2021-05-16 17:08:54 +0300

    feat: add content

--- lesson-4-closures/task1.js
+++ lesson-4-closures/task1.js
@@ -1,21 +1,21 @@
 'use strict';
 
 function createDesk(width, height) {
-  var desk = '';
-  for (var i = 0; i < width; i++) {
-    var substr = '';
-    var br = '\n';
-    for (var j = 0; j < height; j++) {
-      if ((i + j) % 2 === 0) {
-        substr += '#';
-      } else {
-        substr += ' ';
-      }
-    }
-    substr += br;
-    desk += substr;
-  }
-  return desk;
+	var desk = '';
+	for (var i = 0; i < width; i++) {
+		var substr = '';
+		var br = '\n';
+		for (var j = 0; j < height; j++) {
+			if ((i + j) % 2 === 0) {
+				substr += '#';
+			} else {
+				substr += ' ';
+			}
+		}
+		substr += br;
+		desk += substr;
+	}
+	return desk;
 }
 
 console.log( createDesk(8, 8) );
@@ -23,20 +23,20 @@ console.log( createDesk(8, 8) );
 
 
 // Начальный вариант
-// var desk = '';
-// for (var i = 0; i < 8; i++) {
-// 	var substr = '';
-// 	var br = '\n';
+var desk = '';
+for (var i = 0; i < 8; i++) {
+	var substr = '';
+	var br = '\n';
 
-// 	for (var j = 0; j < 8; j++) {
-// 		if ((i + j) % 2 === 0) {
-// 			substr += '#';
-// 		} else {
-// 			substr += ' ';
-// 		}
-// 	}
-// 	substr += br;
-// 	desk += substr;
-// }
+	for (var j = 0; j < 8; j++) {
+		if ((i + j) % 2 === 0) {
+			substr += '#';
+		} else {
+			substr += ' ';
+		}
+	}
+	substr += br;
+	desk += substr;
+}
 
-// console.log(desk);
+console.log(desk);
