00aa245b12083fef6464cb3078c2570ee38a9034
Alena Astravukh <helen151204@gmail.com>
2021-05-16 15:23:50 +0300

    feat: add solution

---
+++ lesson-4-closures/task1.js
@@ -0,0 +1,42 @@
+'use strict';
+
+function createDesk(width, height) {
+	var desk = '';
+	for (var i = 0; i < width; i++) {
+		var substr = '';
+		var br = '\n';
+		for (var j = 0; j < height; j++) {
+			if ((i + j) % 2 === 0) {
+				substr += '#';
+			} else {
+				substr += ' ';
+			}
+		}
+		substr += br;
+		desk += substr;
+	}
+	return desk;
+}
+
+console.log( createDesk(8, 8) );
+
+
+
+// Начальный вариант
+// var desk = '';
+// for (var i = 0; i < 8; i++) {
+// 	var substr = '';
+// 	var br = '\n';
+
+// 	for (var j = 0; j < 8; j++) {
+// 		if ((i + j) % 2 === 0) {
+// 			substr += '#';
+// 		} else {
+// 			substr += ' ';
+// 		}
+// 	}
+// 	substr += br;
+// 	desk += substr;
+// }
+
+// console.log(desk);
