10c4944a41b3b8387e6cd150d5424f56531eec14
Alena Astravukh <helen151204@gmail.com>
2021-05-29 23:11:35 +0300

    feat: correct syntax

--- lesson-7-oop/task1.js
+++ lesson-7-oop/task1.js
@@ -1,47 +1,47 @@
 'use strict';
 
 (function () {
-function Question(question, answer1, answer2, answer3, numOfCorrectAnswer) {
-this.question = question;
-this.answers = [answer1, answer2, answer3];
-this.indexOfCorrectAnswer = numOfCorrectAnswer - 1;
-}
-
-Question.prototype.showQuestion = function () {
-  console.log(this.question);
-  for (var i = 0; i < this.answers.length; i++) {
-    console.log((i + 1) +'. ' + this.answers[i]);
+  function Question(question, answer1, answer2, answer3, numOfCorrectAnswer) {
+  this.question = question;
+  this.answers = [answer1, answer2, answer3];
+  this.indexOfCorrectAnswer = numOfCorrectAnswer - 1;
   }
-}
 
-Question.prototype.isCorrectAnswer = function(i) {
-  if ((i - 1) == this.indexOfCorrectAnswer) {
-    console.log('It\'s a correct answer!');
-  } else {
-    console.log('Your answer is wrong!');
+  Question.prototype.showQuestion = function () {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log((i + 1) +'. ' + this.answers[i]);
+    }
   }
-}
 
-var quest1 = new Question('What is Expression in programming?', 'It is a unit of code that results in a value', 'It is emotion',
-  'I don\'t know', 1);
-var quest2 = new Question('What is Coersion in programming?', 'I don\'t know', 'It is a process of converting a value from one type to another',
-  'Some default value', 2);
-var quest3 = new Question('Execution context is...', 'a program that reads your code', 'a wrapper to help manage the code that is running',
-  'I don\'t know', 2);
-
-var questionsArray = new Array(quest1, quest2, quest3);
-var index = Math.floor(Math.random() * 3);
-
-questionsArray[index].showQuestion();
-
-var userAnswer = '';
-function getUserAnswer() {
-  while (userAnswer == '' || userAnswer == null || isNaN(userAnswer)) {
-    userAnswer = +prompt('Choose the right answer!');
+  Question.prototype.isCorrectAnswer = function(i) {
+    if ((i - 1) == this.indexOfCorrectAnswer) {
+      console.log('It\'s a correct answer!');
+    } else {
+      console.log('Your answer is wrong!');
+    }
   }
-  return userAnswer;
-}
-getUserAnswer();
 
-questionsArray[index].isCorrectAnswer(userAnswer);
+  var quest1 = new Question('What is Expression in programming?', 'It is a unit of code that results in a value', 'It is emotion',
+    'I don\'t know', 1);
+  var quest2 = new Question('What is Coersion in programming?', 'I don\'t know', 'It is a process of converting a value from one type to another',
+    'Some default value', 2);
+  var quest3 = new Question('Execution context is...', 'a program that reads your code', 'a wrapper to help manage the code that is running',
+    'I don\'t know', 2);
+
+  var questionsArray = new Array(quest1, quest2, quest3);
+  var index = Math.floor(Math.random() * 3);
+
+  questionsArray[index].showQuestion();
+
+  var userAnswer = '';
+  function getUserAnswer() {
+    while (userAnswer == '' || userAnswer == null || isNaN(userAnswer)) {
+      userAnswer = +prompt('Choose the right answer!');
+    }
+    return userAnswer;
+  }
+  getUserAnswer();
+
+  questionsArray[index].isCorrectAnswer(userAnswer);
 })();
