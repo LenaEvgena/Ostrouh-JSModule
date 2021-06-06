6ef57849c50a45120b3401a42b5229e7610944b6
Alena Astravukh <helen151204@gmail.com>
2021-06-07 01:47:12 +0300

    feat: add solution

--- lesson-9-dom/task1.js
+++ lesson-9-dom/task1.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function HashStorage() {
+  this.storage = {};// приватный хэш для значений
+}
+
+HashStorage.prototype.addValue = function(key, value) {
+  this.storage[key] = value;
+}
+
+HashStorage.prototype.getValue = function(key) {
+  if (!(key in this.storage)) {
+    return undefined;
+  } else {
+    return this.storage[key];
+  }
+}
+
+HashStorage.prototype.deleteValue = function(key) {
+  if (!(key in this.storage)) {
+    return false;
+  } else {
+    delete this.storage[key];
+    return true;
+  }
+}
+
+HashStorage.prototype.getKeys = function() {
+  return Object.keys(this.storage);
+}
+
+
+
