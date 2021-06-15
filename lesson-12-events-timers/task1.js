05dd4ec569f59951739f116ace592a868af8a98a
Alena Astravukh <helen151204@gmail.com>
2021-06-15 21:22:11 +0300

    feat: add content

--- lesson-12-events-timers/task1.js
+++ lesson-12-events-timers/task1.js
@@ -1,1 +1,25 @@
 'use strict';
+
+var deg = 6;
+var hourA = document.getElementById('hr');
+var minA = document.getElementById('mn');
+var secA = document.getElementById('sc');
+var time = document.querySelector('.time');
+
+setInterval(function() {
+  var day = new Date();
+  var seconds = day.getSeconds() * deg;
+  var minutes = day.getMinutes() * deg;
+  var hours = (day.getHours() + (1 / 60) * day.getMinutes()) * 30;
+
+  hourA.style.transform = 'rotate(' + hours + 'deg)';
+  hourA.style.transformOrigin = 'left center';
+  minA.style.transform = 'rotate(' + minutes + 'deg)';
+  minA.style.transformOrigin = 'left center';
+  secA.style.transform = 'rotate(' + seconds + 'deg)';
+  secA.style.transformOrigin = 'left center';
+
+  time.innerHTML = day.toLocaleTimeString();
+});
+
+
