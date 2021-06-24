8d50ce49c88a6d6714efbf4b9738c1dcc78418e5
Alena Astravukh <helen151204@gmail.com>
2021-06-24 12:31:28 +0300

    feat: finish analog clock

--- lesson-13-timers-svg-canvas/canvas/task2.js
+++ lesson-13-timers-svg-canvas/canvas/task2.js
@@ -13,74 +13,80 @@ ctx.translate(baseRadius, baseRadius); // делаем центр часов в 
 const bigRadius = baseRadius * 0.95; // радиус циферблата
 const smallRadius = baseRadius * 0.75; // радиус расположения цифр
 const numCircleRadius = baseRadius * 0.11; //радиус кружков с цифрами
-const dotSize = baseRadius * 0.05; //размер точки в центре часов
+const dotSize = baseRadius * 0.04; //размер точки в центре часов
 const font = bigRadius * 0.1 + "px Roboto";//размер шрифта
 
 createWatch();
-// setInterval(tickTimer, 0);
+setInterval(createWatch, 0);
 
 // UI
 
 function createWatch() {
   drawClockFace();
+  updateTime();
   createDecorativeDot(dotSize);
-
 }
 
 function drawClockFace() { //окружность с кружками цифр
   //arc(x,y, r, a1,a2, d)
+  ctx.save();
+  ctx.beginPath();
   ctx.arc(0, 0, bigRadius, 0, 2 * Math.PI);
-  ctx.fillStyle = '#ebd8cb';
-  ctx.fill();
+  ctx.fillStyle = '#f1d7c7';
   ctx.strokeStyle = '#F1CDB3';
   ctx.lineWidth = 10;
+  ctx.fill();
   ctx.stroke();
 
   for (let number = 1; number <= 12; number++) {
       let angle = number / 12 * Math.PI * 2;
-      let x = ((bigRadius - smallRadius) / 2 - dotSize * 2) + Math.round(Math.sin(angle) * smallRadius);
-      let y = ((bigRadius - smallRadius) / 2 - dotSize * 2) - Math.round(Math.cos(angle) * smallRadius);
+      let x = ((bigRadius - smallRadius) / 2 - dotSize * 2.5) + Math.round(Math.sin(angle) * smallRadius);
+      let y = ((bigRadius - smallRadius) / 2 - dotSize * 2.5) - Math.round(Math.cos(angle) * smallRadius);
       drawHourCircle(x, y);
       drawNumbers(number, x, y);
   }
+  ctx.restore();
 }
 
 function drawHourCircle(x, y) { //кружки цифр
+  ctx.save();
   ctx.fillStyle = '#F1CDB3';
   ctx.beginPath();
-  ctx.arc(x, y, numCircleRadius, 0, 2 * Math.PI); ////////////////
+  ctx.arc(x, y, numCircleRadius, 0, 2 * Math.PI);
   ctx.fill();
+  ctx.restore();
 }
 
-
 function drawNumbers(number, x, y) { //цифры
+  ctx.save();
   ctx.font = font;
   ctx.fillStyle = 'black';
   ctx.textBaseline = 'middle';
   ctx.textAlign = 'center';
   ctx.fillText(number, x, y);
+  ctx.restore();
 }
 
-function createArrow(arrowType, arrowWidth, arrowHeight, arrowColor) {
-  let arrow = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-  arrow.setAttribute('x1', baseRadius / 2);
-  arrow.setAttribute('y1', baseRadius / 2);
-  arrow.setAttribute('x2', baseRadius / 2 + arrowWidth);
-  arrow.setAttribute('y2', baseRadius / 2);
-  arrow.setAttribute('stroke-linecap', 'round');
-  arrow.setAttribute('stroke', arrowColor);
-  arrow.setAttribute('stroke-width', arrowHeight);
-  arrow.id = arrowType;
-  arrow.setAttribute('transform', `translate(${baseRadius, baseRadius})`);
-  arrow.style.transformOrigin = 'center';
-  return arrow;
+function createArrow(ang, arrowWidth, arrowLength) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.lineWidth = arrowWidth;
+  ctx.lineCap = 'round';
+  ctx.moveTo(0, 0);
+  ctx.rotate(ang); //повернулись
+  ctx.lineTo(0, -arrowLength); //отрисовали
+  ctx.stroke(); //зарисовали
+  ctx.rotate(-ang); // в исходное
+  ctx.restore();
 }
 
 function createDecorativeDot(size) {
+  ctx.save();
   ctx.beginPath();
+  // ctx.moveTo(0, 0);
   ctx.arc(0, 0, size, 0, 2 * Math.PI);
-  ctx.fillStyle = 'red';
   ctx.fill();
+  ctx.restore();
 }
 
 function createDigitalWatch() { //цифровые часы
@@ -111,30 +117,31 @@ function createDigits() { //цифры
 }
 // Logic
 
-function tickTimer() {
+function updateTime() {
   let now = new Date();
-  let thisSecond = now.getSeconds();
-  let thisMinute = now.getMinutes();
-  let thisHour = now.getHours();
-  updateWatch(thisHour, thisMinute, thisSecond);
-  updateDigitalWatch(now);
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  let second = now.getSeconds();
+  //hours
+  let thisHourRotate = (hour * Math.PI / 6 ) + (minute * Math.PI / (6 * 60));
+  createArrow(thisHourRotate, bigRadius * 0.045, bigRadius * 0.5);
+  //minutes
+  let thisMinuteRotate = (minute * Math.PI / 30);
+  createArrow(thisMinuteRotate, bigRadius * 0.035, bigRadius * 0.75);
+  //seconds
+  let thisSecondRotate = second * Math.PI / 30;
+  createArrow(thisSecondRotate, bigRadius * 0.01, bigRadius * 0.85);
 }
+// function tickTimer() {
+  //   let now = new Date();
+//   let thisSecond = now.getSeconds();
+//   let thisMinute = now.getMinutes();
+//   let thisHour = now.getHours();
+//   updateWatch(thisHour, thisMinute, thisSecond);
+//   updateDigitalWatch(now);
+// }
 
-function updateWatch(hour, minute, second) {
-  let thisSecondRotate = (second / 60) * 360 - 90;
-  let thisMinuteRotate = (minute) / 60 * 360 - 90;
-  let thisHourRotate = (hour + minute / 60) / 12 * 360 - 90;
-  rotateHandle('seconds', thisSecondRotate);
-  rotateHandle('minutes', thisMinuteRotate);
-  rotateHandle('hours', thisHourRotate);
-}
-
-function rotateHandle(handle, degree) {
-  let arrow = document.getElementById(`${handle}`);
-  arrow.style.transform = `rotate(${degree}deg)`;
-}
-
-function updateDigitalWatch(day) {
-  let time = document.getElementById('digitWatch');
-  time.textContent = day.toLocaleTimeString();
-}
+// function updateDigitalWatch(day) {
+//   let time = document.getElementById('digitWatch');
+//   time.textContent = day.toLocaleTimeString();
+// }
