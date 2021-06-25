a62e710251150a02f0b766f69180ec1bf513965f
Alena Astravukh <helen151204@gmail.com>
2021-06-25 15:33:51 +0300

    feat: add content

--- lesson-14-animation/task1.js
+++ lesson-14-animation/task1.js
@@ -1,6 +1,5 @@
 'use strict';
 var RequestAnimationFrame =
-      // находим, какой метод доступен
       window.requestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.mozRequestAnimationFrame ||
@@ -10,33 +9,79 @@ var RequestAnimationFrame =
         window.setTimeout(callback, 1000 / 60);
       };
 
-var BallH =
-  {
-    PosX: 300,
-    PosY: 200,
-    SpeedX: 4,
-    SpeedY: 2,
-    Width: 50,
-    Height: 50,
-
-    Update: function () {
-      var BallObj = document.getElementById('IBall');
-      BallObj.style.left = this.PosX + "px";
-      BallObj.style.top = this.PosY + "px";
-    }
-  };
-
 var AreaH =
   {
     Width: 600,
     Height: 400
   }
 
-function Start() {
-  RequestAnimationFrame(Tick);
+var BallH =
+  {
+    PosX: 275,
+    PosY: 175,
+    SpeedX: 2,
+    SpeedY: 1,
+    Width: 50,
+    Height: 50,
+    Update: function () {
+      var Ball = document.getElementById('IBall');
+      Ball.style.left = this.PosX + "px";
+      Ball.style.top = this.PosY + "px";
+    }
+  };
+
+var Racket1 =
+{
+  PosX: 0,
+  PosY: 135,
+  SpeedY: 0,
+  Width: 15,
+  Height: 130,
+  Update: function () {
+    var Racket1 = document.getElementById('racket1');
+    Racket1.style.top = this.PosY + "px";
+  },
+  // moveUp: function () {
+  //   var Racket1 = document.getElementById('racket1');
+  //   Racket1.PosY -= Racket1.SpeedY;
+  //   Racket1.style.top = this.PosY + "px";
+  // },
+  // moveDown: function () {
+  //   var Racket1 = document.getElementById('racket1');
+  //   Racket1.PosY += Racket1.SpeedY;
+  //   Racket1.style.top = this.PosY + "px";
+  // }
 }
 
-function Tick() {
+var Racket2 =
+{
+  PosX: 585,
+  PosY: 135,
+  SpeedY: 0,
+  Width: 15,
+  Height: 130,
+  Update: function () {
+    var Racket2 = document.getElementById('racket2');
+    Racket2.style.top = this.PosY + "px";
+  },
+  // moveUp: function () {
+  //   var Racket2 = document.getElementById('racket2');
+  //   Racket2.PosY -= Racket2.SpeedY;
+  //   Racket2.style.top = this.PosY + "px";
+  // },
+  // moveDown: function () {
+  //   var Racket2 = document.getElementById('racket2');
+  //   Racket2.PosY += Racket2.SpeedY;
+  //   Racket2.style.top = this.PosY + "px";
+  // }
+}
+
+function Start() {
+  RequestAnimationFrame(throwBall);
+  RequestAnimationFrame(moveRacket);
+}
+
+function throwBall() {
   BallH.PosX += BallH.SpeedX;
   // вылетел ли мяч правее стены
   if (BallH.PosX + BallH.Width > AreaH.Width) {
@@ -60,8 +105,58 @@ function Tick() {
     BallH.PosY = 0;
   }
   BallH.Update();
-  RequestAnimationFrame(Tick);
+  RequestAnimationFrame(throwBall);
 }
 BallH.Update();
 
+document.addEventListener('keydown', moveRacket, false);
+// document.addEventListener('keyup', stopRacket, false);
+
+function moveRacket(event) {
+  if (event.code === 'ShiftLeft') {
+    Racket1.SpeedY = 1;
+    Racket1.PosY -= Racket1.SpeedY;
+  }
+  if (event.code === 'ControlLeft') {
+    Racket1.SpeedY = 1;
+    Racket1.PosY += Racket1.SpeedY;
+  }
+  if (event.code === 'ArrowUp') {
+    Racket2.SpeedY = 1;
+    Racket2.PosY -= Racket2.SpeedY;
+  }
+  if (event.code === 'ArrowDown') {
+    Racket2.SpeedY = 1;
+    Racket2.PosY += Racket2.SpeedY;
+  }
+  checkPosition(Racket1);
+  checkPosition(Racket2);
+  RequestAnimationFrame(moveRacket);
+}
+
+// function stopRacket(whatRacket) {
+  //   whatRacket.SpeedY = 0;
+  //   whatRacket.Update();
+  //   // RequestAnimationFrame(dropBall);
+  // }
+
+function checkPosition(racket) {
+  // сместилась ли ракетка выше потолка?
+  if (racket.PosY < 0) {
+    racket.PosY = 0;
+  }
+  // сместилась ли ракетка ниже пола?
+  if (racket.PosY + racket.Height > AreaH.Height) {
+    racket.PosY = AreaH.Height - racket.Height;
+  }
+  // сместилась ли ракетка выше потолка?
+  if (racket.PosY < 0) {
+    racket.PosY = 0;
+  }
+  // сместилась ли ракетка ниже пола?
+  if (racket.PosY + racket.Height > AreaH.Height) {
+    racket.PosY = AreaH.Height - racket.Height;
+  }
+  racket.Update();
+}
 
